4b3c26fbc3315ecc98a1b8ac23a87ce2350fde89
Maciej Piech <m.piech@piechdesign.pl>
2024-02-18 03:57:55 +0100

    Added Identity migration

--- Backend/Eventodo.Infrastructure/Migrations/20240218025521_Identity.Designer.cs
+++ Backend/Eventodo.Infrastructure/Migrations/20240218025521_Identity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Eventodo.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Eventodo.Infrastructure.Migrations
 {
     [DbContext(typeof(EventodoDbContext))]
-    partial class EventodoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240218025521_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
